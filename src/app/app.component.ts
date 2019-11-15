4951ffdd43da54381760f9ddaf1067f93ce7d0b2
vincent F <visant@msn.com>
2019-11-15 11:31:32 +0100

    Added github package dep for ngx-soap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgxSoapService, Client } from '@visant/ngx-soap';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-lib-proj';
+  client: Client;
+
+  constructor(private soap: NgxSoapService) {
+    this.soap.createClient('assets/calculator.wsdl')
+      .then(client => {
+        console.log('Client', client);
+        this.client = client;
+      })
+      .catch(err => console.log('Error', err));
+  }
+
+  sum() {
+    // this.client.setEndpoint('http://www.dneonline.com/calculator.asmx');
+
+    this.client.call('Add', { intA: 1, intB: 2}).subscribe(res => {
+      console.log(res.responseBody);
+      console.log(res.result.AddResult);
+    }, err => console.log(err));
+
+  }
 }
